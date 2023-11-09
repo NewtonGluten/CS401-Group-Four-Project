5077c879d693c250c738a8e75084b7c1305d0538
jnijjareb <124319700+jnijjareb@users.noreply.github.com>
2023-11-08 16:17:51 -0800

    added UserStorage constructor and User; prepared for authenticator class

---
+++ src/User.java
@@ -0,0 +1,41 @@
+enum UserStatus {
+	Online, Offline, Away, Busy
+}
+
+enum UserRole {
+	Normal, IT
+}
+
+public class User {
+	private String id;
+	private String password;
+	private UserStatus status;
+	private UserRole role;
+	
+	public User(String id, String password, UserRole role) {
+		this.id = id;
+		this.password = password;
+		this.role = role;
+		this.status = UserStatus.Offline;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public UserStatus getStatus() {
+		return status;
+	}
+	
+	public UserRole getRole() {
+		return role;
+	}
+	
+	public void setStatus(UserStatus status) {
+		this.status = status;
+	}
+}
---
+++ src/UserStorage.java
@@ -0,0 +1,64 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import java.io.File;
+
+public class UserStorage {
+	private Map<String, List<String>> userIdRooms;
+	private Map<String, User> users;
+	
+	public UserStorage() {
+		userIdRooms = new HashMap<String, List<String>>();
+		users = new HashMap<String, User>();
+		try {
+			File file = new File("credentials.txt");
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				String credentials[] = line.split(":");
+				if (credentials.length != 3)
+					//TODO: what should we do for corrupt data
+					continue;
+				//TODO: how should we store user roles
+				users.putIfAbsent(credentials[0], new User(
+						credentials[0], 
+						credentials[1],
+						credentials[2].charAt(0) == 'I' ? UserRole.IT : UserRole.Normal
+				));
+			}
+			scanner.close();
+		} catch(Exception e) {
+			System.out.println("credentials.txt was not able to be opened");
+			e.printStackTrace();
+		}
+		
+		try {
+			File file = new File("userrooms.txt");
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				//first string is the user id, second is a comma-separated list of room id's
+				String info[] = line.split(":");
+				if (info.length != 2)
+					//TODO: what should we do for corrupt data
+					continue;
+				
+				//list of rooms user is in, may be empty
+				List<String> rooms = new ArrayList<String>();
+				for (String roomId : info[1].split(",")) {
+					rooms.add(roomId);
+				}
+				
+				userIdRooms.putIfAbsent(info[0], rooms);
+			}
+			scanner.close();
+		} catch(Exception e) {
+			System.out.println("userrooms.txt was not able to be opened");
+			e.printStackTrace();
+		}
+		
+	}
+}
