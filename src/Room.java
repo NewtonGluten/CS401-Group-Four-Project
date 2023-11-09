45e5c8f003d7d5351f4e81282e9465ec824d4c83
jnijjareb <124319700+jnijjareb@users.noreply.github.com>
2023-11-09 13:27:34 -0800

    implemented most of Room

---
+++ src/Room.java
@@ -0,0 +1,62 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Room {
+	private String id;
+	private Date creationDate;
+	private List<String> users;
+	private ChatHistory chatHistory;
+	private boolean empty;
+	
+	public Room(String[] users) {
+		this.users = new ArrayList<String>();
+		for (String userId : users) {
+			this.users.add(userId);
+		}
+		empty = this.users.isEmpty();
+	}
+	
+	public boolean isEmpty() {
+		return empty;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	
+	public List<String> getUsers() {
+		return users;
+	}
+	
+	//TODO: how does this work
+	public String getLog() {
+		return null;
+	}
+	
+	public List<ChatMessage> getMessages() {
+		return chatHistory.getMessages();
+	}
+	
+	public void addUser(String userId) {
+		users.add(userId);
+		empty = users.isEmpty();
+	}
+	
+	public void removeUser(String userId) {
+		users.remove(userId);
+		empty = users.isEmpty();
+	}
+	
+	public void addMessage(ChatMessage message) {
+		chatHistory.addMessage(null);
+	}
+	
+	public void setMessageStatus(String messageId, MessageStatus status) {
+		chatHistory.setMessageStatus(messageId, status);
+	}
+}
