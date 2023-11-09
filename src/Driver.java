a756862447d1810358fddff4c8002abe40d06a10
jnijjareb <124319700+jnijjareb@users.noreply.github.com>
2023-11-08 16:38:49 -0800

    updated test driver to use authenticator and user storage

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,16 @@
+import java.util.List;
+
 public class Driver {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    UserStorage userStorage = new UserStorage();
+    Authenticator authenticator = new Authenticator(userStorage);
+    User user = authenticator.authenticate("randomuser", "pass");
+    System.out.println(user.getId() + ' ' + user.getPassword());
+    List<String> rooms = userStorage.getUserRooms("randomuser");
+    for (String roomId : rooms) {
+    	System.out.println(roomId);
+    }
+    
   }
 }
