2b4b3d8640e9ca5059f459ba81edc2e57c1edf80
Hoshikara <anguyen1905@gmail.com>
2023-11-24 23:26:01 -0800

    ClientHandler reader and writer classes

---
+++ src/ClientHandlerReader.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class ClientHandlerReader implements Runnable {
+  private ObjectInputStream objIn;
+  private UpdateManager updateManager;
+
+  public ClientHandlerReader(ObjectInputStream objIn, UpdateManager updateManager) {
+    this.objIn = objIn;
+    this.updateManager = updateManager;
+  }
+
+  public void run() {
+    try {
+      while (true) {
+        // Blocking read
+        Message msg = (Message) objIn.readObject();
+
+        // Pass any messages to the update manager
+        updateManager.handleMessage(msg);
+
+        // Breaking the loop means the thread is done running
+        // In this event, the ClientHandler should carry out the logout process
+        if (msg.getType() == MessageType.Logout) {
+          break;
+        }
+      }
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
