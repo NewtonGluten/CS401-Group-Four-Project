ff89372703d57076a27706a5b2fa89ee6df7b763
Hoshikara <anguyen1905@gmail.com>
2023-11-29 15:19:38 -0800

    make authenticator return descriptive Message

--- src/Authenticator.java
+++ src/Authenticator.java
@@ -5,11 +5,23 @@ public class Authenticator {
 		this.userStorage = userStorage;
 	}
 	
-	public User authenticate(String userId, String password) {
+	public Message authenticate(String userId, String password) {
+		Message msg = new Message(MessageType.Login);
 		User user = userStorage.getUserById(userId);
-		if (user == null || !user.getPassword().equals(password))
-			return null;
-		user.setStatus(UserStatus.Online);
-		return user;
+
+		if (user == null) {
+			msg.setContents("User does not exist");
+		} else if (!user.getPassword().equals(password)) {
+			msg.setContents("Password is incorrect");
+		} else if (user.getStatus() == UserStatus.Online) {
+			msg.setContents("User is already logged in");
+		} else {
+			user.setStatus(UserStatus.Online);
+			msg.setUser(user);
+			msg.setUserId(userId);
+			msg.setUserStatus(UserStatus.Online);
+		}
+
+		return msg;
 	}
 }
