d1b63c0cbe50f016b35a64ef3c133d0863064294
Hoshikara <anguyen1905@gmail.com>
2023-11-30 01:25:53 -0800

    logger methods for specific users and rooms

--- src/Logger.java
+++ src/Logger.java
@@ -55,4 +55,42 @@ public class Logger {
 		roomStorage.removeUser(roomId, userId);
 		userStorage.removeRoom(userId, roomId);
 	}
+
+	public String getLogsForUser(String userId) {
+		List<String> roomIds = userStorage.getUserRooms(userId);
+		String s = "";
+		for (String roomId : roomIds) {
+			Room room = roomStorage.getRoomById(roomId);
+			ChatHistory chatHistory = room.getChatHistory();
+			List<ChatMessage> messages = chatHistory.getMessagesFromUser(userId);
+
+			if (messages.isEmpty()) {
+				continue;
+			}
+
+			s += "Room ID: " + room.getId() + "\n";	
+
+			for (ChatMessage message : messages) {
+				s += message + "\n";
+			}
+
+			s += "================================\n";
+		}
+
+		if (s.isEmpty()) {
+			s = "No logs found for " + userId;
+		}
+
+		return s;
+	}
+
+	public String getLogForRoom(String roomId) {
+		Room room = roomStorage.getRoomById(roomId);
+
+		if (room == null) {
+			return "Room with ID " + roomId + " does not exist";
+		}
+
+		return room.toString();
+	}
 }
