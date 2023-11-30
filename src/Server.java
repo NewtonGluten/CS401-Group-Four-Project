99b402894130d3e7f26845c71944ba64151e9065
Hoshikara <anguyen1905@gmail.com>
2023-11-30 02:01:13 -0800

    basic server ui

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.net.*;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 // Server class
 class Server {
   public static void main(String[] args) {
@@ -13,19 +17,13 @@ class Server {
     Logger logger = new Logger(rooms, users);
     UpdateManager updateManager = new UpdateManager(rooms, users, logger);
     
-    //TODO: remove debug print
-    System.out.println("users: " + users.hashCode() + "\n" +
-    				   "authenticator: " + authenticator.hashCode() + "\n" +
-    				   "rooms: " + rooms.hashCode() + "\n" +
-    				   //"logger: " + logger.hashCode() + "\n" +
-    				   "\n" );
-    
-
     try {
 
       // server is listening on port 1234
       server = new ServerSocket(4014);
       server.setReuseAddress(true);
+
+      createWindow(server, logger);
 
       // running infinite loop for getting
       // client request
@@ -33,7 +31,14 @@ class Server {
 
         // socket object to receive incoming client
         // requests
-        Socket client = server.accept();
+        Socket client = null;
+
+        try {
+          client = server.accept();
+        } catch (SocketException e) {
+          // Server was closed
+          break;
+        }
         
         
         //TODO: remove debugging code
@@ -60,10 +65,45 @@ class Server {
       if (server != null) {
         try {
           server.close();
+          logger.save();
+          System.exit(0);
         } catch (IOException e) {
           e.printStackTrace();
         }
       }
     }
+  }
+
+  public static void createWindow(ServerSocket socket, Logger logger) {
+    JFrame window = new JFrame("Server Application");
+    JPanel panel = new JPanel();
+    JButton saveBtn = new JButton("Save");
+    JButton shutdownBtn = new JButton("Shutdown");
+
+    saveBtn.addActionListener(e -> {
+      logger.save();
+    });
+
+    shutdownBtn.addActionListener(e -> {
+      try {
+        socket.close();
+      } catch (IOException e1) {
+        e1.printStackTrace();
+      }
+
+      logger.save();
+
+      System.exit(0);
+    });
+
+    panel.add(saveBtn);
+    panel.add(shutdownBtn);
+    
+    window.setMinimumSize(new java.awt.Dimension(400, 100));
+    window.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    window.setLocationRelativeTo(null);
+    window.add(panel);
+    window.pack();
+    window.setVisible(true);
   }
 }
