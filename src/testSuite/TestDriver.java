5c7d32a450ae494c1b78a6fdfec637c890491f76
greekKostaN <Kosta.a.nikolaou1@gmail.com>
2023-12-04 11:44:37 -0800

    Created testSuite package containing all JUnit tests.

---
+++ src/testSuite/TestDriver.java
@@ -0,0 +1,18 @@
+package testSuite;
+import commsProj.*;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+//you can also right click on Test.java and do Run As > JUnit Test
+public class TestDriver {
+   public static void main(String[] args) {
+      Result result = JUnitCore.runClasses(Test.class);
+
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+		
+      System.out.println(result.wasSuccessful());
+   }
+}
