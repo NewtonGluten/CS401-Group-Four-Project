fd87011d5258eb05b5ab14a41fa96863767c8b0e
jnijjareb <124319700+jnijjareb@users.noreply.github.com>
2023-12-04 11:14:13 -0800

    tidied up tests

--- src/UserTest.java
+++ src/UserTest.java
@@ -6,12 +6,13 @@ public class UserTest {
 	
 	@Test
 	public void testConstrutor() {
+		//test normal user creation
 		User user = new User("bob", "password", UserRole.Normal);
 		assertNotNull(user);
-		
+		//test IT user creation
 		User IT = new User("jeff", "long password", UserRole.IT);
 		assertEquals(IT.getRole(), UserRole.IT);
-		
+		//test getters
 		assertEquals(user.getId(), "bob");
 		assertEquals(user.getPassword(), "password");
 		assertEquals(user.getRole(), UserRole.Normal);
@@ -20,13 +21,15 @@ public class UserTest {
 	
 	@Test
 	public void testStatus() {
+		//create a user
 		User user = new User("bob", "password", UserRole.Normal);
+		//change the status to Online and check
 		user.setStatus(UserStatus.Online);
 		assertEquals(user.getStatus(), UserStatus.Online);
-		
+		//test changing to Away
 		user.setStatus(UserStatus.Away);
 		assertEquals(user.getStatus(), UserStatus.Away);
-		
+		//test changing to Busy
 		user.setStatus(UserStatus.Busy);
 		assertEquals(user.getStatus(), UserStatus.Busy);
 	}
